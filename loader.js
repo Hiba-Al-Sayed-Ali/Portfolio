e76b5655d6d8222db616dc93f0da6d6b7706210d
Hiba Al Sayed Ali <hibasy2007@gmail.com>
2024-12-07 20:08:24 +0200

    Portfolio

---
+++ loader.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    const mainContent = document.getElementById('main-content');
+    
+
+    loader.style.display = 'none';
+    
+
+    mainContent.style.display = 'block';
+});
+
